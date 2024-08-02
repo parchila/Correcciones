0374734903710694cc2603adde87045b09a86217
parchila <parchila.gordillo@gmail.com>
2024-08-02 15:20:30 -0600

    Codigo 1

---
+++ 1.c
@@ -0,0 +1,13 @@
+//Corregir este codigo para que imprima la suma de a y b
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    a = 5;
+    b = 10;
+    c = a + b;
+    printf("%d + %d = %d\n", a, b, c);
+    return 0;
+}
